aa9a318bbea508442581d7f5a30dad0bc51dc54a
üsâme_aw <osamasaedawad@gmail.com>
2024-05-16 13:30:25 +0300

    jsut for demo delete later

--- glados_ws/src/glados_bringup/launch/threeD_map.launch.py
+++ glados_ws/src/glados_bringup/launch/threeD_map.launch.py
@@ -18,22 +18,22 @@ def generate_launch_description():
             parameters=[
                 {"frame_id": "odom"},
                 {"base_frame_id": "base_footprint"},
-                {"resolution": 0.05},
+                {"resolution": 0.001},
                 {"sensor_model.max_range": -1.0},
                 {"sensor_model.hit": 0.8},
                 {"sensor_model.miss": 0.2},
                 {"filter_speckles": True},
                 
-                {"filter_ground_plane": True},
-                {"ground_filter.distance": 0.10},
-                {"ground_filter.angle": 0.15},
-                {"ground_filter.plane_distance": 0.10},
+                # {"filter_ground_plane": True},
+                # {"ground_filter.distance": 0.10},
+                # {"ground_filter.angle": 0.15},
+                # {"ground_filter.plane_distance": 0.10},
                 
-                {"point_cloud_min_z": -99.0},
-                {"point_cloud_max_z": 99.0},
+                # {"point_cloud_min_z": -99.0},
+                # {"point_cloud_max_z": 99.0},
                 
-                {"occupancy_min_z": -99.0},
-                {"occupancy_max_z": 21.0},
+                # {"occupancy_min_z": -99.0},
+                # {"occupancy_max_z": 21.0},
                 
             ],
             
