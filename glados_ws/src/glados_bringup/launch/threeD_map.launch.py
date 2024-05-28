acee5983cf74799398daf8005b35fe780945d52c
üsâme_aw <osamasaedawad@gmail.com>
2024-05-28 22:57:34 +0300

    add ground filter to octomap

--- glados_ws/src/glados_bringup/launch/threeD_map.launch.py
+++ glados_ws/src/glados_bringup/launch/threeD_map.launch.py
@@ -11,29 +11,29 @@ def generate_launch_description():
     return LaunchDescription([
         
         Node(
-            package='octomap_server',
+            package='glados_octomap_server',
             executable='octomap_server_node',
             name='mapping',
             output='screen',
             parameters=[
                 {"frame_id": "odom"},
                 {"base_frame_id": "base_footprint"},
-                {"resolution": 0.001},
+                {"resolution": 0.03},
                 {"sensor_model.max_range": -1.0},
                 {"sensor_model.hit": 0.8},
                 {"sensor_model.miss": 0.2},
                 {"filter_speckles": True},
                 
-                # {"filter_ground_plane": True},
+                {"filter_ground_plane": True},
                 # {"ground_filter.distance": 0.10},
                 # {"ground_filter.angle": 0.15},
-                # {"ground_filter.plane_distance": 0.10},
+                {"ground_filter.plane_distance": 0.10},
                 
-                # {"point_cloud_min_z": -99.0},
-                # {"point_cloud_max_z": 99.0},
+                {"point_cloud_min_z": -99.0},
+                {"point_cloud_max_z": 99.0},
                 
-                # {"occupancy_min_z": -99.0},
-                # {"occupancy_max_z": 21.0},
+                {"occupancy_min_z": -99.0},
+                {"occupancy_max_z": 0.22},
                 
             ],
             
